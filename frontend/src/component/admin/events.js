1144be721feb1a29508f5d2be57150497365e9c7
Chetan Pandey <chetanpandey1266@gmail.com>
2020-07-30 11:02:35 +0530

    Add events dynamically

--- frontend/src/component/admin/events.js
+++ frontend/src/component/admin/events.js
@@ -1,32 +1,34 @@
-import React, { Component } from 'react';
+import React, { Component, useState, setState } from 'react';
 import ModalContent from './modal.js'
 import TextModal from './textmodal.js'
 import data from './details.js'
 
-class Events extends Component {
-    render() { 
-        console.log(data.Events)
-        const elements = data.Events
-
+function Events() {
+        const [elements, setelements] = useState(['first', 'second', 'third', 'four'])
         var items = []
-
-        for( var [index, value] of elements.entries()){
+        console.log(elements)
+        for(var [index, value] of elements.entries()){
             items.push(
                 <div class="card text-white bg-dark mb-3" id={index} style={{width:"18em", margin:"10px"}}>
                     <div class="card-header">
-                    <h3 style={{fontWeight:"bold", textTransform:"uppercase", fontSize:"1.2vw"}}>{value.title}</h3>
+                    <h3 style={{fontWeight:"bold", textTransform:"uppercase", fontSize:"1.2vw"}}>{value}</h3>
                         <ModalContent />
                     </div>
                     <div class="card-body">
                         <img class="card-img" />
-                        <h5 class="card-title" style={{fontWeight:"bold", fontSize:"1.2vw"}}>{value.details}</h5>
+                        <h5 class="card-title" style={{fontWeight:"bold", fontSize:"1.2vw"}}>{}</h5>
                         <p class="card-text"></p>
                         <button type="button" class="btn btn-primary">Publish</button>
                     </div>
                 </div>
             )
         }
-        
+        console.log(items)
+
+
+        const addItems = () =>{
+            setelements([ ...elements, "NEW"])
+        }
 
         return (  
         <div>
@@ -36,12 +38,12 @@ class Events extends Component {
                     {items}
                 </div>
                 <center>
-                    <button type="button" class="btn btn-primary btn-lg" onClick={()=> elements.push('five')}>Create New Event</button>
+                    <button type="button" class="btn btn-primary btn-lg" onClick={() => addItems() }>Create New Event</button>
                 </center>
             </div>
         </div>
           );
-    }
+    
 }
  
 export default Events;
